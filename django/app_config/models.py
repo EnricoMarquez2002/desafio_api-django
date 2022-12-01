abd6ce962011ec82adda33d27a97d4b09451c430
EnricoMarquez2002 <118749543+EnricoMarquez2002@users.noreply.github.com>
2022-12-01 10:12:16 -0300

    Delete django/app_config directory

--- django/app_config/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class BaseModel(models.Model):
-    ativo = models.BooleanField("Ativo",default=True)
-    data_criacao = models.DateField('Data de criação', auto_now_add=True)
-    data_modificacao = models.DateField("Data de modificação", auto_now=True)
-
-    class Meta:
-        abstract=True
